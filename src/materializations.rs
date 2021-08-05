2861daa4bce04d714cb409222440d409ab69a7ca
brurucy <brurucy@gmail.com>
2021-08-05 21:18:04 +0300

    Deduplicated lots of code.

--- src/materializations.rs
+++ src/materializations.rs
@@ -9,30 +9,30 @@ use timely::dataflow::Scope;
 use timely::order::Product;
 
 type EncodedTriple = (usize, usize, usize);
+type EncodedTripleByS = (usize, (usize, usize));
 
-pub fn rdfs<G>(
+fn tbox_spo_sco_materialization<G>(
     tbox: &Collection<G, EncodedTriple>,
-    abox: &Collection<G, EncodedTriple>,
     outer: &mut G,
-) -> (Collection<G, EncodedTriple>, Collection<G, EncodedTriple>)
+) -> Collection<G, EncodedTripleByS>
 where
     G: Scope,
     G::Timestamp: Lattice,
 {
-    let tbox = outer.region_named("Tbox transitive rules", |inn| {
+    outer.region_named("Tbox transitive rules", |inn| {
         let tbox = tbox.enter(inn);
 
         let tbox_by_o = tbox.map(|(s, p, o)| (o, (p, s)));
 
-        let tbox_by_s = tbox.map(|(s, p, o)| (s, (p, o)));
+        let tbox_by_s = tbox.map(|(s, p, o)| ((s, (p, o))));
 
         let sco_ass_by_o = tbox_by_o.filter(|(_, (p, _))| p == &0usize);
 
-        let sco_ass_by_s = sco_ass_by_o.map(|(o, (p, s))| (s, (p, o)));
+        let sco_ass_by_s = sco_ass_by_o.map(|(o, (p, s))| ((s, (p, o))));
 
         let spo_ass_by_o = tbox_by_o.filter(|(_, (p, _))| p == &1usize);
 
-        let spo_ass_by_s = spo_ass_by_o.map(|(o, (p, s))| (s, (p, o)));
+        let spo_ass_by_s = spo_ass_by_o.map(|(o, (p, s))| ((s, (p, o))));
 
         let (spo, sco) = inn.iterative::<usize, _, _>(|inner| {
             let sco_var =
@@ -73,20 +73,101 @@ where
         });
 
         tbox_by_s.concat(&sco).concat(&spo).leave()
-    });
+    })
+}
 
-    let tbox_by_s = tbox.arrange_by_key();
-    let sco_assertions = tbox_by_s.filter(|s, (p, o)| p == &0usize);
-    let spo_assertions = tbox_by_s.filter(|s, (p, o)| p == &1usize);
-    let domain_assertions = tbox_by_s.filter(|s, (p, o)| p == &2usize);
-    let range_assertions = tbox_by_s.filter(|s, (p, o)| p == &3usize);
-    let general_trans_assertions = tbox_by_s.filter(|s, (p, o)| o == &5usize);
-    let inverse_of_assertions = tbox_by_s.filter(|s, (p, o)| p == &6usize);
+fn abox_sco_type_materialization<G>(
+    tbox_sco_assertions: &Collection<G, EncodedTripleByS>,
+    abox_class_assertions: &Collection<G, EncodedTripleByS>,
+    outer: &mut G,
+) -> Collection<G, EncodedTripleByS>
+where
+    G: Scope,
+    G::Timestamp: Lattice,
+{
+    outer.region_named("Abox transitive class rules", |inn| {
+        let sco_assertions = tbox_sco_assertions.enter(inn);
+        let class_assertions = abox_class_assertions.enter(inn);
+
+        let class_materialization = inn.iterative::<usize, _, _>(|inner| {
+            let sco_type_var =
+                iterate::SemigroupVariable::new(inner, Product::new(Default::default(), 1));
+
+            let sco_type_new = sco_type_var.distinct();
+
+            let sco_type_arr = sco_type_new.arrange_by_key();
+
+            let class_assertions = class_assertions.enter(inner);
+
+            let sco_tbox_ass = sco_assertions.enter(inner);
+
+            let sco_iter_step = sco_tbox_ass
+                .join_core(&sco_type_arr, |_key, &(_sco, y), &(z, type_)| {
+                    Some((y, (z, type_)))
+                });
+
+            sco_type_var.set(&class_assertions.concat(&sco_iter_step));
+
+            sco_type_new.leave()
+        });
+
+        class_materialization.leave()
+    })
+}
+
+fn abox_domain_and_range_type_materialization<G>(
+    tbox_domain_assertions: &Collection<G, EncodedTripleByS>,
+    tbox_range_assertions: &Collection<G, EncodedTripleByS>,
+    abox_property_assertions: &Collection<G, EncodedTripleByS>,
+    outer: &mut G,
+) -> (
+    Collection<G, EncodedTripleByS>,
+    Collection<G, EncodedTripleByS>,
+)
+where
+    G: Scope,
+    G::Timestamp: Lattice,
+{
+    outer.region_named("Domain and Range type rules", |inner| {
+        let property_assertions_arr = abox_property_assertions.enter(inner).arrange_by_key();
+
+        let domain_assertions = tbox_domain_assertions.enter(inner);
+
+        let domain_type = domain_assertions
+            .join_core(&property_assertions_arr, |_, &(_, x), &(y, _)| {
+                Some((x, (y, 4usize)))
+            });
+
+        let range_assertions = tbox_range_assertions.enter(inner);
+
+        let range_type = range_assertions
+            .join_core(&property_assertions_arr, |_, &(_, x), &(_, z)| {
+                Some((x, (z, 4usize)))
+            });
+
+        (domain_type.leave(), range_type.leave())
+    })
+}
+
+pub fn rdfs<G>(
+    tbox: &Collection<G, EncodedTriple>,
+    abox: &Collection<G, EncodedTriple>,
+    outer: &mut G,
+) -> (Collection<G, EncodedTriple>, Collection<G, EncodedTriple>)
+where
+    G: Scope,
+    G::Timestamp: Lattice,
+{
+    let tbox = tbox_spo_sco_materialization(tbox, outer);
+
+    let sco_assertions = tbox.filter(|((s, (p, o)))| p == &0usize);
+    let spo_assertions = tbox.filter(|((s, (p, o)))| p == &1usize);
+    let domain_assertions = tbox.filter(|((s, (p, o)))| p == &2usize);
+    let range_assertions = tbox.filter(|((s, (p, o)))| p == &3usize);
 
     let class_assertions = abox
         .map(|(s, p, o)| (o, (s, p)))
         .filter(|(o, (s, p))| p == &4usize);
-
     let property_assertions = abox
         .map(|(s, p, o)| (p, (s, o)))
         .filter(|(p, (s, o))| p != &4usize);
@@ -94,8 +175,6 @@ where
     let property_materialization = outer.region_named("Abox transitive property rules", |inn| {
         let property_assertions = property_assertions.enter(inn);
         let spo_assertions = spo_assertions.enter(inn);
-        let general_trans_assertions = general_trans_assertions.enter(inn);
-        let inverse_of_assertions = inverse_of_assertions.enter(inn);
 
         let property_materialization = inn.iterative::<usize, _, _>(|inner| {
             let spo_type_gen_trans_inv_var =
@@ -123,60 +202,19 @@ where
 
     let property_assertions = property_materialization.concat(&property_assertions);
 
-    let property_assertions_arr = property_assertions.arrange_by_key();
-
-    let (domain_type, range_type) = outer.region_named("Domain and Range type rules", |inner| {
-        let property_assertions_arr = property_assertions_arr.enter(inner);
-
-        let domain_assertions = domain_assertions.enter(inner);
-
-        let domain_type = domain_assertions
-            .join_core(&property_assertions_arr, |_, &(_, x), &(y, _)| {
-                Some((x, (y, 4usize)))
-            });
-
-        let range_assertions = range_assertions.enter(inner);
-
-        let range_type = range_assertions
-            .join_core(&property_assertions_arr, |_, &(_, x), &(_, z)| {
-                Some((x, (z, 4usize)))
-            });
-
-        (domain_type.leave(), range_type.leave())
-    });
+    let (domain_type, range_type) = abox_domain_and_range_type_materialization(
+        &domain_assertions,
+        &range_assertions,
+        &property_assertions,
+        outer,
+    );
 
     let class_assertions = class_assertions
         .concatenate(vec![domain_type, range_type])
         .consolidate();
 
-    let class_materialization = outer.region_named("Abox transitive class rules", |inn| {
-        let sco_assertions = sco_assertions.enter(inn);
-        let class_assertions = class_assertions.enter(inn);
-
-        let class_materialization = inn.iterative::<usize, _, _>(|inner| {
-            let sco_type_var =
-                iterate::SemigroupVariable::new(inner, Product::new(Default::default(), 1));
-
-            let sco_type_new = sco_type_var.distinct();
-
-            let sco_type_arr = sco_type_new.arrange_by_key();
-
-            let class_assertions = class_assertions.enter(inner);
-
-            let sco_tbox_ass = sco_assertions.enter(inner);
-
-            let sco_iter_step = sco_tbox_ass
-                .join_core(&sco_type_arr, |_key, &(_sco, y), &(z, type_)| {
-                    Some((y, (z, type_)))
-                });
-
-            sco_type_var.set(&class_assertions.concat(&sco_iter_step));
-
-            sco_type_new.leave()
-        });
-
-        class_materialization.leave()
-    });
+    let class_materialization =
+        abox_sco_type_materialization(&sco_assertions, &class_assertions, outer);
 
     let class_assertions = class_assertions.concat(&class_materialization);
 
@@ -195,7 +233,7 @@ where
             .leave()
     });
 
-    let tbox = tbox.map(|(s, (p, o))| (s, p, o));
+    let tbox = tbox.map(|((s, (p, o)))| (s, p, o));
 
     (tbox, abox)
 }
@@ -209,69 +247,14 @@ where
     G: Scope,
     G::Timestamp: Lattice,
 {
-    let tbox = outer.region_named("Tbox transitive rules", |inn| {
-        let tbox = tbox.enter(inn);
+    let tbox = tbox_spo_sco_materialization(tbox, outer);
 
-        let tbox_by_o = tbox.map(|(s, p, o)| (o, (p, s)));
-
-        let tbox_by_s = tbox.map(|(s, p, o)| (s, (p, o)));
-
-        let sco_ass_by_o = tbox_by_o.filter(|(_, (p, _))| p == &0usize);
-
-        let sco_ass_by_s = sco_ass_by_o.map(|(o, (p, s))| (s, (p, o)));
-
-        let spo_ass_by_o = tbox_by_o.filter(|(_, (p, _))| p == &1usize);
-
-        let spo_ass_by_s = spo_ass_by_o.map(|(o, (p, s))| (s, (p, o)));
-
-        let (spo, sco) = inn.iterative::<usize, _, _>(|inner| {
-            let sco_var =
-                iterate::SemigroupVariable::new(inner, Product::new(Default::default(), 1));
-            let spo_var =
-                iterate::SemigroupVariable::new(inner, Product::new(Default::default(), 1));
-
-            let sco_new = sco_var.distinct();
-
-            let spo_new = spo_var.distinct();
-
-            let sco_new_arr = sco_new.arrange_by_key();
-            let spo_new_arr = spo_new.arrange_by_key();
-
-            let sco_ass_by_s = sco_ass_by_s.enter(inner);
-            let spo_ass_by_s = spo_ass_by_s.enter(inner);
-
-            let sco_ass_by_o = sco_ass_by_o.enter(inner);
-            let spo_ass_by_o = spo_ass_by_o.enter(inner);
-
-            let sco_ass_by_o_arr = sco_ass_by_o.arrange_by_key();
-            let spo_ass_by_o_arr = spo_ass_by_o.arrange_by_key();
-
-            let sco_iter_step = sco_ass_by_o_arr
-                .join_core(&sco_new_arr, |&_, &(p, s), &(_, o_prime)| {
-                    Some((s, (p, o_prime)))
-                });
-
-            let spo_iter_step = spo_ass_by_o_arr
-                .join_core(&spo_new_arr, |&_, &(p, s), &(_, o_prime)| {
-                    Some((s, (p, o_prime)))
-                });
-
-            sco_var.set(&sco_ass_by_s.concat(&sco_iter_step));
-            spo_var.set(&spo_ass_by_s.concat(&spo_iter_step));
-
-            (sco_new.leave(), spo_new.leave())
-        });
-
-        tbox_by_s.concat(&sco).concat(&spo).leave()
-    });
-
-    let tbox_by_s = tbox.arrange_by_key();
-    let sco_assertions = tbox_by_s.filter(|s, (p, o)| p == &0usize);
-    let spo_assertions = tbox_by_s.filter(|s, (p, o)| p == &1usize);
-    let domain_assertions = tbox_by_s.filter(|s, (p, o)| p == &2usize);
-    let range_assertions = tbox_by_s.filter(|s, (p, o)| p == &3usize);
-    let general_trans_assertions = tbox_by_s.filter(|s, (p, o)| o == &5usize);
-    let inverse_of_assertions = tbox_by_s.filter(|s, (p, o)| p == &6usize);
+    let sco_assertions = tbox.filter(|(s, (p, o))| p == &0usize);
+    let spo_assertions = tbox.filter(|(s, (p, o))| p == &1usize);
+    let domain_assertions = tbox.filter(|(s, (p, o))| p == &2usize);
+    let range_assertions = tbox.filter(|(s, (p, o))| p == &3usize);
+    let general_trans_assertions = tbox.filter(|(s, (p, o))| o == &5usize);
+    let inverse_of_assertions = tbox.filter(|(s, (p, o))| p == &6usize);
 
     let class_assertions = abox
         .map(|(s, p, o)| (o, (s, p)))
@@ -300,9 +283,7 @@ where
             let inverse_ass = inverse_of_assertions.enter(inner);
 
             let left_inverse_ass = inverse_ass.clone();
-            let right_inverse_ass = inverse_ass
-                .as_collection(|s, (p, o)| (*o, (*p, *s)))
-                .arrange_by_key();
+            let right_inverse_ass = inverse_ass.map(|(s, (p, o))| (o, (p, s))).arrange_by_key();
 
             let spo_iter_step = spo_ass
                 .join_core(&spo_type_gen_trans_inv_arr, |_key, &(_spo, b), &(x, y)| {
@@ -346,64 +327,21 @@ where
         property_materialization.leave()
     });
 
-    let property_assertions = property_materialization
-        //.inspect(|x| println!("SPO and Gen Trans materialization{:?}", x))
-        .concat(&property_assertions);
+    let property_assertions = property_materialization.concat(&property_assertions);
 
-    let property_assertions_arr = property_assertions.arrange_by_key();
-
-    let (domain_type, range_type) = outer.region_named("Domain and Range type rules", |inner| {
-        let property_assertions_arr = property_assertions_arr.enter(inner);
-
-        let domain_assertions = domain_assertions.enter(inner);
-
-        let domain_type = domain_assertions
-            .join_core(&property_assertions_arr, |_, &(_, x), &(y, _)| {
-                Some((x, (y, 4usize)))
-            });
-
-        let range_assertions = range_assertions.enter(inner);
-
-        let range_type = range_assertions
-            .join_core(&property_assertions_arr, |_, &(_, x), &(_, z)| {
-                Some((x, (z, 4usize)))
-            });
-
-        (domain_type.leave(), range_type.leave())
-    });
+    let (domain_type, range_type) = abox_domain_and_range_type_materialization(
+        &domain_assertions,
+        &range_assertions,
+        &property_assertions,
+        outer,
+    );
 
     let class_assertions = class_assertions
         .concatenate(vec![domain_type, range_type])
         .consolidate();
 
-    let class_materialization = outer.region_named("Abox transitive class rules", |inn| {
-        let sco_assertions = sco_assertions.enter(inn);
-        let class_assertions = class_assertions.enter(inn);
-
-        let class_materialization = inn.iterative::<usize, _, _>(|inner| {
-            let sco_type_var =
-                iterate::SemigroupVariable::new(inner, Product::new(Default::default(), 1));
-
-            let sco_type_new = sco_type_var.distinct();
-
-            let sco_type_arr = sco_type_new.arrange_by_key();
-
-            let class_assertions = class_assertions.enter(inner);
-
-            let sco_tbox_ass = sco_assertions.enter(inner);
-
-            let sco_iter_step = sco_tbox_ass
-                .join_core(&sco_type_arr, |_key, &(_sco, y), &(z, type_)| {
-                    Some((y, (z, type_)))
-                });
-
-            sco_type_var.set(&class_assertions.concat(&sco_iter_step));
-
-            sco_type_new.leave()
-        });
-
-        class_materialization.leave()
-    });
+    let class_materialization =
+        abox_sco_type_materialization(&sco_assertions, &class_assertions, outer);
 
     let class_assertions = class_assertions.concat(&class_materialization);
 
@@ -422,7 +360,7 @@ where
             .leave()
     });
 
-    let tbox = tbox.map(|(s, (p, o))| (s, p, o));
+    //let tbox = tbox.map(|((s, (p, o)))|(s, p, o));
 
-    (tbox, abox)
+    (tbox.map(|(s, (p, o))| (s, p, o)), abox)
 }
--- tests/materialization_tests.rs
+++ tests/materialization_tests.rs
@@ -3,9 +3,95 @@ use differential_dataflow::operators::arrange::ArrangeBySelf;
 use differential_dataflow::trace::cursor::CursorDebug;
 use differential_dataflow::trace::TraceReader;
 use differential_reasoner::load_encode_triples::{load3enc, loadkvenc, read_file};
-use differential_reasoner::materializations::*;
+use differential_reasoner::materializations::{rdfs, rdfspp};
 use std::collections::BTreeMap;
 use timely::dataflow::operators::probe::Handle;
+
+#[test]
+fn rdfs_test() {
+    let tbox_triples = load3enc("./encoded_data/test/tbox.ntenc");
+    let abox_triples = load3enc("./encoded_data/test/abox.ntenc");
+
+    let (mut tbox_summaries, mut abox_summaries) = timely::execute_directly(move |worker| {
+        let mut tbox_probe = Handle::new();
+        let mut abox_probe = Handle::new();
+
+        let (mut tbox_input_stream, mut abox_input_stream, mut tbox_trace, mut abox_trace) = worker
+            .dataflow::<usize, _, _>(|outer| {
+                let (mut _abox_in, abox) = outer.new_collection::<(usize, usize, usize), isize>();
+                let (mut _tbox_in, tbox) = outer.new_collection::<(usize, usize, usize), isize>();
+
+                let (tbox, abox) = rdfs(&tbox, &abox, outer);
+
+                tbox.probe_with(&mut tbox_probe);
+                abox.probe_with(&mut abox_probe);
+
+                let tbox_arr = tbox.arrange_by_self();
+                let abox_arr = abox.arrange_by_self();
+
+                (_tbox_in, _abox_in, tbox_arr.trace, abox_arr.trace)
+            });
+
+        tbox_triples.for_each(|triple| {
+            tbox_input_stream.insert((triple.0, triple.1, triple.2));
+        });
+
+        tbox_input_stream.advance_to(1);
+        tbox_input_stream.flush();
+        worker.step_while(|| tbox_probe.less_than(tbox_input_stream.time()));
+
+        abox_triples.for_each(|triple| {
+            abox_input_stream.insert((triple.0, triple.1, triple.2));
+        });
+        abox_input_stream.advance_to(1);
+        abox_input_stream.flush();
+        worker.step_while(|| abox_probe.less_than(abox_input_stream.time()));
+
+        let (mut tbox_cursor, tbox_storage) = tbox_trace.cursor();
+        let (mut abox_cursor, abox_storage) = abox_trace.cursor();
+
+        (
+            tbox_cursor.to_vec(&tbox_storage),
+            abox_cursor.to_vec(&abox_storage),
+        )
+    });
+
+    let encoding_map_file = loadkvenc("./encoded_data/test/encoding_mapping.kv");
+
+    let mut encoding_map = BTreeMap::<usize, String>::new();
+
+    for key_value_par in encoding_map_file {
+        let (key, value) = key_value_par;
+        encoding_map.insert(key, value.clone());
+    }
+
+    let tbox_size = tbox_summaries.len();
+
+    let abox_size = abox_summaries.len();
+
+    for summary in abox_summaries.drain(..) {
+        let encoded_triple = summary.0 .0;
+
+        let s = encoding_map.get(&encoded_triple.0).unwrap();
+        let p = encoding_map.get(&encoded_triple.1).unwrap();
+        let o = encoding_map.get(&encoded_triple.2).unwrap();
+
+        println!("Abox entry: {:?}", (s, p, o))
+    }
+
+    for summary in tbox_summaries.drain(..) {
+        let encoded_triple = summary.0 .0;
+
+        let s = encoding_map.get(&encoded_triple.0).unwrap();
+        let p = encoding_map.get(&encoded_triple.1).unwrap();
+        let o = encoding_map.get(&encoded_triple.2).unwrap();
+
+        println!("Tbox entry: {:?}", (s, p, o))
+    }
+
+    assert_eq!(tbox_size, 29);
+    assert_eq!(abox_size, 15);
+}
 
 #[test]
 fn rdfspp_test() {
